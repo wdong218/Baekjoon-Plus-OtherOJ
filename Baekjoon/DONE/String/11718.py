2947a196e84475b8973a59f4900d4ac02428c2a0
wdong218 <wdh8890@naver.com>
2024-11-18 23:56:26 +0900

    11718

--- Baekjoon/DONE/String/11718.py
+++ Baekjoon/DONE/String/11718.py
@@ -3,3 +3,8 @@ while True:
         print(input())
     except EOFError:
         break
+#무한반복문을 사용하여 한 줄씩 입력받아 출력한다.
+
+#만약 더이상 입력이 없다면 EOFError 예외처리로 무한반복문을 나오게 된다.
+
+#EOFError : 'End of File Eorror'의 약자로 파일에서 더 이상 읽을 내용이 없거나 입력 값이 없을 때 발생하는 에러.
