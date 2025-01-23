23268175c23beab1aeb678afe0571935489ab863
wdong218 <wdh8890@naver.com>
2025-01-23 21:05:36 +0900

    ING : 33151

--- Baekjoon/ING/33151.py
+++ Baekjoon/ING/33151.py
@@ -1,32 +1,25 @@
 n,k = map(int,input().split())
 grid = [[0 for _ in range(n)] for _ in range(n)]
-if k < 2*n -1:
-    print("-1")
-else:
-    k -= 2*n -1 # 기본적인 1 구성 후 남은 코인 갯수
-    for i in range(n):  # 대각선 기본적인 1 구성
-        for j in range(n):
-            if i == j:  # 대각선
-                grid[i][j] += 1
-    if k % 2 != 0:
-        result = -1
+if n % 2 == 0: # n이 짝수 인 경우
+    if k % 2 != 0 or n**2//2 < k: #n이 짝수 인 경우 무조건 k도 짝수여야 격자판 채우기 가능 or 격자판의 갯수 // 2 보다 k가 크거나 같아야 채우기 가능
+        print("-1")
+    else: # 그외의 경우는 무조건 격자판 채우기 가능
+        #기본 격자판 구성 짝수의 경우 1가지 격자판만 고려해도 됨
+        for i in range(n):
+            for j in range(n):
+                if j % 2 == 0 and i % 2 == 0:
+                    grid[i][j] = 1
+                if i % 2 != 0 and j % 2 != 0:
+                    grid[i][j] = 1
+        k -= n**2//2
+        while k > 0:
+
+
+
+        print(grid)
+
+else: #n이 홀수 인 경우
+    if n**2//2 < k: #n이 홀수라면 이 경우만 따지면 됨 나머지는 다 가능
+        print("-1")
     else:
-        count = k // 2
-
-
-
-# 1 0 1
-# 0 1 0
-# 1 0 1
-
-
-# # 2*n -1 개의 동전일 경우 무조건 가능 그 이하의 경우 불가능
-# # 2 2 2 2 채우고
-# 1 2 1
-# 2 1 2
-# 1 2 1
-#
-# #그 다음에도 2씩 늘어나야됨 만약 남은 동전이 홀수 개라면 -1 출력
-# 3 2 1
-# 2 1 2
-# 1 2 1
+        pass
