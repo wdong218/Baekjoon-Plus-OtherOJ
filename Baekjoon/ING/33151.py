fc1290967ebbb763aac3eb69cc11df0b881b7042
wdong218 <wdh8890@naver.com>
2025-01-24 19:34:19 +0900

    ING : 33151

--- Baekjoon/ING/33151.py
+++ Baekjoon/ING/33151.py
@@ -1,25 +1,142 @@
-n,k = map(int,input().split())
-grid = [[0 for _ in range(n)] for _ in range(n)]
-if n % 2 == 0: # n이 짝수 인 경우
-    if k % 2 != 0 or n**2//2 < k: #n이 짝수 인 경우 무조건 k도 짝수여야 격자판 채우기 가능 or 격자판의 갯수 // 2 보다 k가 크거나 같아야 채우기 가능
+import sys
+input = sys.stdin.readline
+n, k = map(int, input().split())
+grid = [[0] * n for _ in range(n)]  # numpy 대신 파이썬 2차원 리스트
+
+if n % 2 == 0:  # n이 짝수인 경우
+    if k % 2 != 0 or (n ** 2) // 2 > k:
         print("-1")
-    else: # 그외의 경우는 무조건 격자판 채우기 가능
-        #기본 격자판 구성 짝수의 경우 1가지 격자판만 고려해도 됨
+    else:
+        # 기본 격자판 구성 갯수
+        base = k // (6 * n)
+
         for i in range(n):
             for j in range(n):
                 if j % 2 == 0 and i % 2 == 0:
-                    grid[i][j] = 1
+                    grid[i][j] = base
+                    k -= base
+                if i % 2 == 0 and j % 2 != 0:
+                    grid[i][j] = base + 1
+                    k -= (base + 1)
                 if i % 2 != 0 and j % 2 != 0:
-                    grid[i][j] = 1
-        k -= n**2//2
-        while k > 0:
+                    grid[i][j] = base
+                    k -= base
+                if i % 2 != 0 and j % 2 == 0:
+                    grid[i][j] = base + 1
+                    k -= (base + 1)
 
-
-
+        y = 0
+        x = 0
         print(grid)
+        print(k)
+        while k > 0:
+            if y > n - 1:
+                y = 0
+                x = 0
+            # x+1이 범위를 넘지 않고, grid[y][x] > grid[y][x+1] 이면 오른쪽 칸에 +2
+            if x + 1 < n and grid[y][x] > grid[y][x + 1]:
+                grid[y][x + 1] += 2
+                k -= 2
+            else:
+                grid[y][x] += 2
+                k -= 2
+            x += 2
+            if x > n - 2:
+                x = 0
+                y += 1
 
-else: #n이 홀수 인 경우
-    if n**2//2 < k: #n이 홀수라면 이 경우만 따지면 됨 나머지는 다 가능
+# 홀수가 제일 문제 !!!@!@!@ 나중에 다시 풀기 ~~@~@~!@~!2₩
+else:  # n이 홀수인 경우
+    if (n ** 2) // 2 > k:
         print("-1")
     else:
-        pass
+        a = (n ** 2) // 2
+        b = a + 1
+        base = k // (a + 2 * b)
+
+        if k % 2 == 0:  # k가 짝수인 경우
+            for i in range(n):
+                for j in range(n):
+                    if i % 2 == 0 and j % 2 != 0:
+                        if base == 0:
+                            grid[i][j] = 1
+                            k -= 1
+                        else:
+                            grid[i][j] = base
+                            k -= base
+
+                    if i % 2 == 0 and j % 2 == 0 and base != 0:
+                        grid[i][j] = base + 1
+                        k -= (base + 1)
+
+                    if i % 2 != 0 and j % 2 == 0:
+                        if base == 0:
+                            grid[i][j] = 1
+                            k -= 1
+                        else:
+                            grid[i][j] = base
+                            k -= base
+
+                    if i % 2 != 0 and j % 2 != 0 and base != 0:
+                        grid[i][j] = base + 1
+                        k -= (base + 1)
+
+
+            q = min(grid[0][0], grid[0][1])
+            for i in range(n):
+                if k <= 0:
+                    break
+                for j in range(n):
+                    if k <= 0:
+                        break
+                    if grid[i][j] == q:
+                        grid[i][j] += 2
+                        k -= 2
+
+        else:  # k가 홀수인 경우
+            a = (n ** 2) // 2 + 1
+            b = a - 1
+            base = k // (a + 2 * b)
+            print(base)
+
+            for i in range(n):
+                for j in range(n):
+                    if i % 2 == 0 and j % 2 == 0:
+                        if base == 0:
+                            grid[i][j] = 1
+                            k -= 1
+                        else:
+                            grid[i][j] = base
+                            k -= base
+
+                    if i % 2 == 0 and j % 2 != 0 and base != 0:
+                        grid[i][j] = base + 1
+                        k -= base + 1
+
+                    if i % 2 != 0 and j % 2 != 0:
+                        if base == 0:
+                            grid[i][j] = 1
+                            k -= 1
+                        else:
+                            grid[i][j] = base
+                            k -= base
+
+                    if i % 2 != 0 and j % 2 == 0 and base != 0:
+                        grid[i][j] = base + 1
+                        k -= base + 1
+            print(k)
+
+            q = min(grid[0][0],grid[0][1])
+            for i in range(n):
+                if k <= 0:
+                    break
+                for j in range(n):
+                    if k <= 0:
+                        break
+                    if grid[i][j] == q:
+                        grid[i][j] += 2
+                        k -= 2
+
+# 결과 출력
+for row in grid:
+    print(*row)
