416422477f1a0c7bdac2c94c8135c8884149439e
wdong218 <wdh8890@naver.com>
2025-03-31 16:47:23 +0900

    ING : 11725

---
+++ Baekjoon/ING/11725.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+graph = [[] for _ in range(n+1)]
+for _ in range(n-1):
+    a,b = map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+print(graph)
