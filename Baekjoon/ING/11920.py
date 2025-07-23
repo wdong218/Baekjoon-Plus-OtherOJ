c401751ce961063ca3b7df7dbe402544e2e5ad26
wdong218 <wdh8890@naver.com>
2025-07-23 16:50:50 +0900

    start

--- Baekjoon/ING/11920.py
+++
@@ -1,8 +0,0 @@
-import sys
-import heapq
-n,k = map(int,sys.stdin.readline().split())
-num_list = list(map(int,sys.stdin.readline().split()))
-arr = num_list[k:]
-for i in range(k):
-    heapq.heappush(arr, num_list[i])
-print(*arr)
